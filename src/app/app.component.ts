03ad7945ff257938826b61324c9705b20e87b44c
Pierre Bretéché <pbreteche@dawan.fr>
2019-04-29 17:23:51 +0200

    Create task list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,22 @@ import {Task, TaskStatus} from './task';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  task: Task = {
+  tasks: Array<Task> = [{
     name: 'Apprendre Angular',
     status: TaskStatus.Doing,
     deadLine: new Date('2019-05-02')
-  };
+  }, {
+    name: 'Apprendre React',
+    status: TaskStatus.Doing,
+    deadLine: new Date('2019-04-19')
+  }, {
+    name: 'Apprendre Vue',
+    status: TaskStatus.Doing,
+    deadLine: new Date('2019-06-14')
+  }, {
+    name: 'Apprendre le Cobol',
+    status: TaskStatus.Doing,
+    deadLine: new Date('1965-09-12')
+  }];
+  currentTask = this.tasks[0];
 }
