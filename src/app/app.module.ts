6a17a6005fb3fc1309968ea66006cafdfffb869f
Pierre Bretéché <pbreteche@dawan.fr>
2019-05-14 10:50:32 +0200

    début routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavListComponent } from './nav-list/nav-list.component';
 import { DetailComponent } from './detail/detail.component';
 import { CreateComponent } from './create/create.component';
 import { StatComponent } from './stat/stat.component';
+
+const appRoutes: Routes = [
+  { path: 'todolist', component: NavListComponent },
+  { path: 'create', component: CreateComponent },
+  { path: '', redirectTo: '/todolist', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +27,8 @@ import { StatComponent } from './stat/stat.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
